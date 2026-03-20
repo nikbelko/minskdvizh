d40874b9720ac377307c711b3aae8fd49d2355d7
nikbelko <nikbelko@gmail.com>
2026-03-20 20:18:01 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,13 +21,13 @@ const Footer = () => {
           </span>
         </div>
         <a
-          href="https://t.me/MinskDvizhBot"
+          href="https://t.me/MinskDvizh_bot"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center gap-2 text-sm text-primary font-body font-medium hover:text-accent transition-colors"
         >
           <Send className="h-4 w-4" />
-          @MinskDvizhBot
+          @MinskDvizh_bot
         </a>
       </div>
     </footer>
